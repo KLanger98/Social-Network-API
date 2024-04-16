60a54a8be3f7cf59e074e5c6e071dc1921ad86a2
KL <karl.w.langer@gmail.com>
2024-04-16 18:04:29 +0930

    Initial commit + route comments

---
+++ routes/api/user-route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+
+//Get all users route
+router.route('/').get()
+
+//get a single user by _id and populated thought and friend data
+
+//Post a new user
+
+//Update a user by it's _id
+
+//Delete a user by its _id
